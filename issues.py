bde718578e83e1e157efda2f9d18e8c20c565171
Robert Griffith <robert.griffith@FT-MW1000.local>
2020-06-05 10:39:36 +0100

    f string on multiline query string working

--- issues.py
+++ issues.py
@@ -6,6 +6,13 @@ github_key = os.getenv("REPO_GRAPHQL_TOKEN")
 print(github_key)
 repo_id = "MDEwOlJlcG9zaXRvcnkyNjkwNDA2MDA="
 
+latest_message = """
+    Latest Version splunk-8.0.4-767223ac207f-x64-release.msi available - Splunk Linux Addons
+    NEW VERSION AVAILABLE - Splunk Universal Forwarder 8.0.4-767223ac207f
+"""
+title = "NEW VERSION AVAILABLE - Splunk Universal Forwarder"
+latest_version_splunk_uf = "8.0.4-767223ac207f"
+
 query = """query {
     characters {
         results {
@@ -16,35 +23,48 @@ query = """query {
     }
 }"""
 
-create_issue_mutation_string = """mutation {
+create_issue_mutation_string = f'''
+    mutation {{
     __typename
-    createIssue(input: {repositoryId: "MDEwOlJlcG9zaXRvcnkyNjkwNDA2MDA=", title: "Splunk UF - New Version Available", body: "this is a test body"}) {
+    createIssue(input: {{repositoryId: "MDEwOlJlcG9zaXRvcnkyNjkwNDA2MDA=", title: "Splunk UF - New Version Available", body: "{latest_message}"}}) {{
     clientMutationId
-    }
-}"""
+    }}
+}}'''
 
-issue_list_query_string = """query {
-    repository(owner: "bibbycodes", name: "github-api-test") {
-    issues(first: 100) {
-      edges {
-        node {
-          body
-          title
-          state
-        }
-      }
-    }
-  }
-}"""
+print(create_issue_mutation_string)
 
-url = 'https://api.github.com/graphql'
+# response = requests.post(url, json={'query' : create_issue_mutation_string}, headers={"Authorization": "Bearer {}".format(github_key)})
+def fetch_issues(repo_owner, repo_name):
+  issue_list_query = f"""query {{
+    repository(owner: "{repo_owner}", name: "{repo_name}") {{
+      issues(first: 100) {{
+        edges {{
+          node {{
+            body
+            title
+            state
+          }}
+        }}
+      }}
+    }}
+  }}"""
 
-response = requests.post(url, json={'query' : issue_list_query_string}, headers={"Authorization": "Bearer {}".format(github_key)})
-response_dict = json.loads(response.text)
-issues = response_dict['data']['repository']['issues']['edges']
+  url = 'https://api.github.com/graphql'
+  response = requests.post(url, json={'query' : issue_list_query}, headers={"Authorization": "Bearer {}".format(github_key)})
+  response_dict = json.loads(response.text)
+  return response_dict['data']['repository']['issues']['edges']
 
-for item in issues:
-    issue = item['node']
-    if issue['state'] == "OPEN":
-        print(issue)
+def should_raise(latest_version_splunk_uf, issues):
+    first_time = True
+    should_raise = True
+    for item in issues:
+      issue = item['node']
+      if issue['title'] == "Splunk UF - New Version Available":
+        first_time = False
+        if latest_version_splunk_uf in issue['body']:
+          should_raise = False
 
+    if first_time or should_raise:
+        return True
+
+print(fetch_issues("bibbycodes", "github-api-test"))
